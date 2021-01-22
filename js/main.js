b8d2adc66449782506b93a28c9e09399ee953ee7
John Samuels <johnsamuels1218@gmail.com>
2021-01-22 18:57:34 -0500

    Add handlers for nav buttons, rename profile image, and add public key.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.getElementById('homebutton').onclick = () => {
+    console.log("HERE");
+}
+document.getElementById('blogbutton').onclick = () => {
+    window.location.href = 'https://www.blog.johnsamuels.ninja';
+}
+document.getElementById('gpgbutton').onclick = () => {
+    window.location.href = 'https://www.johnsamuels.ninja/John_Samuels_public.asc';
+}
+document.getElementById('otherbutton').onclick = () => {
+    console.log("HERE");
+}
