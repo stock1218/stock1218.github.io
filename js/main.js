7774986d9d8c8ff277f64d4d2c81398f110472cd
John Samuels <johnsamuels1218@gmail.com>
2021-01-23 16:23:19 -0500

    Add switching between home and other content tabs.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+document.getElementById('homeContent').style.display = "";
+document.getElementById('otherContent').style.display = "none";
+
 document.getElementById('homebutton').onclick = () => {
-    console.log("HERE");
+    document.getElementById('homeContent').style.display = "";
+    document.getElementById('otherContent').style.display = "none";
 }
 document.getElementById('blogbutton').onclick = () => {
     window.location.href = 'https://www.blog.johnsamuels.ninja';
@@ -8,5 +12,6 @@ document.getElementById('gpgbutton').onclick = () => {
     window.location.href = 'https://www.johnsamuels.ninja/John_Samuels_public.asc';
 }
 document.getElementById('otherbutton').onclick = () => {
-    console.log("HERE");
+    document.getElementById('homeContent').style.display = "none";
+    document.getElementById('otherContent').style.display = "";
 }
