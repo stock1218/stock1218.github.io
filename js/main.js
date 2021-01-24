6b8785eb8cba6ab4503f78ecb187ba691b0d2526
John Samuels <johnsamuels1218@gmail.com>
2021-01-23 19:26:16 -0500

    Add content to home and contact tabs, and format code.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
-document.getElementById('homeContent').style.display = "";
-document.getElementById('otherContent').style.display = "none";
+homeNode = document.getElementById('homeContent');
+otherNode = document.getElementById('otherContent');
+contactNode = document.getElementById('contactContent');
 
-document.getElementById('homebutton').onclick = () => {
-    document.getElementById('homeContent').style.display = "";
-    document.getElementById('otherContent').style.display = "none";
+otherNode.style.display = "none";
+contactNode.style.display = "none";
+
+var lastNode = homeNode;
+
+var changeTab = (newNode) => {
+    if (lastNode === newNode) { } else {
+        lastNode.style.display = "none";
+        newNode.style.display = "";
+        lastNode = newNode;
+    }
 }
-document.getElementById('blogbutton').onclick = () => {
+
+document.getElementById('homeButton').onclick = () => {
+    changeTab(homeNode);
+}
+
+document.getElementById('contactButton').onclick = () => {
+    changeTab(contactNode);
+}
+
+document.getElementById('resumeButton').onclick = () => {
+}
+
+document.getElementById('blogButton').onclick = () => {
     window.location.href = 'https://www.blog.johnsamuels.ninja';
 }
-document.getElementById('gpgbutton').onclick = () => {
-    window.location.href = 'https://www.johnsamuels.ninja/John_Samuels_public.asc';
-}
-document.getElementById('otherbutton').onclick = () => {
-    document.getElementById('homeContent').style.display = "none";
-    document.getElementById('otherContent').style.display = "";
+
+document.getElementById('otherButton').onclick = () => {
+    changeTab(otherNode);
 }
